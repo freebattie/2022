cd2687cf851daec1992146f0a82ee4f27ffb07b6
bjarte <22171847+freebattie@users.noreply.github.com>
2022-01-24 11:23:54 +0100

    Step 5 complete

--- code/LectureCode/Session5/TerminalMenu.java
+++ code/LectureCode/Session5/TerminalMenu.java
@@ -1,7 +1,22 @@
 package LectureCode.Session5;
 
-public class TerminalMenu {
-    public static void main(String[] args) {
+import java.util.ArrayList;
 
+public class TerminalMenu {
+    ArrayList<String> options = new ArrayList<>();
+    public static void main(String[] args) {
+        TerminalMenu menu = new TerminalMenu();
+
+        menu.options.add("vis saldo");
+        menu.options.add("Sett inn penger");
+        menu.options.add("ta ut penger");
+        int i = 1;
+        for (var s : menu.options){
+            printt(s,i++);
+        }
+    }
+
+    private static void printt(String s,int i) {
+        System.out.println("valg :"+i +" " + s );
     }
 }
--- code/Tasks/step5/src/AskQuestionsGetAnswers.java
+++ code/Tasks/step5/src/AskQuestionsGetAnswers.java
@@ -8,10 +8,10 @@ import java.util.Scanner;
  *  Task 5
  *          Ask N questions and get N answers
  *
- *  - [ ] Ask how many questions to gather as an int
- *  - [ ] Use a loop to ask for that many questions
- *  - [ ] Store each question received in the questions ArrayList
- *  - [ ] Ask each question and collect answers into the answers ArrayList
+ *  - [X ] Ask how many questions to gather as an int
+ *  - [X ] Use a loop to ask for that many questions
+ *  - [X ] Store each question received in the questions ArrayList
+ *  - [X ] Ask each question and collect answers into the answers ArrayList
  *  - [ ] Loop through each question and output the answer given to that question
  *
  *
@@ -25,22 +25,104 @@ import java.util.Scanner;
  */
 
 public class AskQuestionsGetAnswers {
-    Scanner scanner = new Scanner(System.in);
-
+    Scanner sc = new Scanner(System.in);
     ArrayList<String> questions = new ArrayList<>();
     ArrayList<String> answers = new ArrayList<>();
 
     int questionCount = 0;
 
     void addQuestion(String question) {
+       if (questionCount > 0){
+           questions.add(question);
+           questionCount--;
+       }
+    }
+    public void getNumberOfQuestions(){
+        Scanner intSC = new Scanner(System.in);
+        this.questionCount = intSC.nextInt();
+    }
+    public void getQuestion(){
+
+        String q = sc.nextLine();
+        this.addQuestion(q);
+
 
     }
+    public void showQuestionMenu(){
+        printl("Please enter a question, you have  "+questionCount + " questions left to add" );
 
+    }
+    public void showIntroMenu(){
+        printl("How many queston do you want to add?: ");
+
+    }
+    public void printl(String s){
+        System.out.println(s);
+    }
+    public void showAnswerMenu(String s){
+        printl("please enter your awnser for : \n"+s+"?");
+
+    }
     void getAnswer() {
+
+        String a = sc.nextLine();
+        this.answers.add(a);
+
+
+    }
+    private void run() {
+        int q =0;
+        int menu =0;
+        while (true){
+            if (menu == 0){
+                this.showIntroMenu();
+                this.showInputMenu();
+                this.getNumberOfQuestions();
+                menu = 2;
+            }
+            if (menu == 2){
+                this.showQuestionMenu();
+                this.showInputMenu();
+                this.getQuestion();
+
+                if (questionCount == 0)
+                    menu = 3;
+
+            }
+            if (menu == 3){
+
+                this.showAnswerMenu(this.questions.get(q++));
+                this.showInputMenu();
+                this.getAnswer();
+                if (answers.size() == questions.size())
+                    menu = 4;
+            }
+            if (menu == 4){
+                this.printQuestionAndAnswer();
+                break;
+            }
+
+        }
+    }
+
+    private void printQuestionAndAnswer() {
+        for (int i = 0; i < this.questions.size(); i++){
+            printl("Question: " +questions.get(i));
+            printl("Anwser: " + answers.get(i));
+
+        }
+    }
+
+    private void showInputMenu() {
+        System.out.print("Input :");
 
     }
 
     public static void main(String[] args) {
         AskQuestionsGetAnswers FAQ = new AskQuestionsGetAnswers();
+        FAQ.run();
+
     }
+
+
 }
