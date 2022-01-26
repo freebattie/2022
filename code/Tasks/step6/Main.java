45c5a4cb21a4c66fc71bbf54ea286ebe379281fd
bjarte <22171847+freebattie@users.noreply.github.com>
2022-01-26 16:39:05 +0100

    added tekst files to step 6

--- code/Tasks/step6/Main.java
+++ code/Tasks/step6/Main.java
@@ -3,6 +3,8 @@ package Tasks.step6;
 public class Main {
 
     public static void main(String[] args) {
-
+        Program prg = new Program();
+        prg.openFIle("D:\\Skole\\java\\github\\2022\\code\\Tasks\\step6","opg4");
+        prg.printFile();
     }
 }
--- code/Tasks/step6/Program.java
+++ code/Tasks/step6/Program.java
@@ -1,7 +1,37 @@
 package Tasks.step6;
 
-public class Program {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-    Program prg = new Program();
+public class Program {
+Scanner scanner = null;
+    private Scanner tryOpenFile(String path,String name){
+        Scanner scanner=null;
+        try {
+            scanner = new Scanner(new File(path+ "\\"+ name+".txt"));
+            }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        return  scanner;
+    }
+    public void printFile(){
+        try {
+           while (scanner.hasNextLine()){
+               String print ="";
+               print = scanner.nextLine();
+               System.out.println(print);
+           }
+
+        }
+        catch (Exception e){
+            System.out.println( e.getMessage());
+        }
+
+    }
+    public void openFIle(String path,String name){
+        this.scanner = tryOpenFile(path,name);
+    }
 
 }
